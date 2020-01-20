7e212e5c6196338c589872e05123e4a8bfccbd21
nicole sanchez <cherrybomb411@gmail.com>
2020-01-19 16:15:19 -0800

    * SpinnerController.cs: Completed updates for this 03 program
    * Spin.cs:
    * luckyStyles.css:
    * Index.cshtml:

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -6,6 +6,7 @@ namespace LuckySpin.Controllers
     public class SpinnerController : Controller
     {
         Random random = new Random();
+        LuckySpin.Models.Spin spin = new LuckySpin.Models.Spin();
 
         //The Conroller to receive the Luck number from the Route
         public IActionResult Index(int luck = 7) //Default value for luck is 7
@@ -13,15 +14,16 @@ namespace LuckySpin.Controllers
             int a = random.Next(1, 10);
             int b = random.Next(1, 10);
             int c = random.Next(1, 10);
+            LuckySpin.Models.Spin spin = new LuckySpin.Models.Spin();
 
             // Load up the ViewBag for use by the Spinner View "Index.cshtml"
-            ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
-            ViewBag.A = a;
-            ViewBag.B = b;
-            ViewBag.C = c;
-            ViewBag.luck = luck;
+            spin .ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
+            spin.A = a;
+            spin.B = b;
+            spin.C = c;
+            spin.luck = luck;
 
-            return View();
+            return View(spin);
         }
     }
 }
--- LuckySpin/Models/Spin.cs
+++ LuckySpin/Models/Spin.cs
@@ -3,8 +3,19 @@ namespace LuckySpin.Models
 {
     public class Spin
     {
+        public int A;
+        public int B;
+        public int C;
+        public string ImgDisplay;
+        public int luck;
+
         public Spin()
         {
         }
+   
+       
     }
+     
+    
+
 }
