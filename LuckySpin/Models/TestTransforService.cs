5b7a7025384d4d57e96c2a04b618d2e92ff1a8ac
nicole sanchez <cherrybomb411@gmail.com>
2020-01-19 16:57:49 -0800

    Got the program to work with all up dates

---
+++ LuckySpin/Models/TestTransforService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace LuckySpin.Models
+{
+    public class TestTransforService
+    {
+        public TestTransforService()
+        {
+        }
+        public string numberToTest(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return "one";
+
+                case 2:
+                    return "two";
+
+
+                case 3:
+                    return "three";
+
+
+                case 4:
+                    return "four";
+
+
+                case 5:
+                    return "five";
+
+
+                case 6:
+                    return "six";
+
+
+                case 7:
+                    return "seven";
+
+                    ;
+                case 8:
+                    return "eight";
+
+
+                case 9:
+                    return "nine";
+                default: return "error";
+
+
+            }
+        }
+    }
+}
